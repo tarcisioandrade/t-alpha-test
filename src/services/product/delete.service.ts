6df65183c90f3fa4f0179d7a395a8681b1bf89f7
Tarcisio <tarcisioandrade016@gmail.com>
2024-08-09 23:41:38 -0300

    feat: delete product service

---
+++ src/services/product/delete.service.ts
@@ -0,0 +1,6 @@
+import { instance } from '@/lib/instance-axios';
+
+export async function deleteProductAsync(id: number) {
+  const res = await instance.delete(`/products/delete-product/${id}`);
+  return res.data;
+}
